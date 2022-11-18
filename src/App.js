9657e268b682ef18f9378b412551ee300718db62
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-11-17 19:24:08 -0500

    added starter code

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+// import './App.css';
+import Navbar from './components/Navbar';
+import {useState} from "react"
+import About from './components/About';
+import Contact from './components/Contact';
+function App() {
+  const [currentPage, setCurrentPage] = useState("Home")
+  function Navigation () {
+    if (currentPage === "Home") {
+      return <h2>Homepage</h2>
+    } else if (currentPage === "About") {
+      return <About/>
+    } else if (currentPage === "Contact") {
+      return <Contact/>
+    }
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Navbar setPage= {setCurrentPage}/>
+       {Navigation()}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+
+
+
+function Navbar (props) {
+
+    return (
+
+        <nav>
+            <h2>My Portfolio</h2>
+            <ul>
+                <li onClick={ () => props.setPage("Home")}>Home</li>
+                <li onClick={ () => props.setPage("About")}>About</li>
+                <li onClick={ () => props.setPage("Contact")}>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
